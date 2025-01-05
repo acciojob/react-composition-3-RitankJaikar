2366787f92277745a4e6cb378bf09e0da7a6f53d
RitankJaikar <ritank.jaikar@gmail.com>
2025-01-05 11:36:41 +0530

    Submit

--- src/components/App.js
+++ src/components/App.js
@@ -9,10 +9,15 @@ const App = () => {
 
   return (
     <div>
-        <h2 onMouseEnter={() => setTooltip1(true)} onMouseLeave={() => setTooltip1(false)} className="tooltip">Hover over me</h2>
-        {tooltip1 && <Tooltip>This is a tooltip</Tooltip>}
-        <p onMouseEnter={() => setTooltip2(true)} onMouseLeave={() => setTooltip2(false)} className="tooltip">Hover over me to see another tooltip</p>
-        {tooltip2 && <Tooltip>This is another tooltip</Tooltip>}
+        <h2 onMouseEnter={() => setTooltip1(true)} onMouseLeave={() => setTooltip1(false)} className="tooltip">
+          Hover over me
+          {tooltip1 && <Tooltip>This is a tooltip</Tooltip>}
+        </h2>
+        
+        <p onMouseEnter={() => setTooltip2(true)} onMouseLeave={() => setTooltip2(false)} className="tooltip">
+          Hover over me to see another tooltip
+          {tooltip2 && <Tooltip>This is another tooltip</Tooltip>}
+        </p>
     </div>
   )
 }
